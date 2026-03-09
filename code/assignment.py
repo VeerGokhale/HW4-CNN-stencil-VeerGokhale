084a58f9af9667a7b0cb164cf7631d89b82f29e7
VeerGokhale <veergokhale@gmail.com>
2026-03-09 17:49:47 -0400

    Task3

--- code/assignment.py
+++ code/assignment.py
@@ -25,19 +25,34 @@ def train(model, optimizer, train_inputs, train_labels):
     :param train_labels: train labels, shape (num_labels,)
     :return: average training accuracy for this epoch
     '''
-    num_batches = 10
+    model.train()
+
+    perm = torch.randperm(train_inputs.shape[0])
+    train_inputs = train_inputs[perm]
+    train_labels = train_labels[perm]
+
+    num_batches = (train_inputs.shape[0] + model.batch_size - 1) // model.batch_size
+    total_acc = 0.0
 
     with tqdm(range(num_batches), desc="  train", unit="batch", leave=False,
               bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}]{postfix}") as pbar:
         for batch_idx in pbar:
+            batch_inputs, batch_labels = get_next_batch(
+                batch_idx, train_inputs, train_labels, model.batch_size
+            )
 
-            pass
+            optimizer.zero_grad()
 
-            # Update the progress bar with current loss and accuracy
-            # pbar.set_postfix(loss=f"{loss.item():.4f}", acc=f"{batch_acc:.3f}")
+            logits = model(batch_inputs)
+            loss = model.loss(logits, batch_labels)
+            batch_acc = model.accuracy(logits, batch_labels)
 
-    # TODO: Return average accuracy across all batches
-    raise NotImplementedError
+            loss.backward()
+            optimizer.step()
+
+            total_acc += batch_acc.item()
+
+    return total_acc / num_batches
 
 
 def test(model, test_inputs, test_labels):
@@ -51,9 +66,31 @@ def test(model, test_inputs, test_labels):
         test accuracy: average accuracy across all batches
         test preds: all of the model's predictions for each of the test inputs
     """
-    # NOTE: You might find it helpful to copy some of the printing logic from train()
-    # NOTE: Don't forget to put your model in eval mode!
-    raise NotImplementedError
+    model.eval()
+
+    num_batches = test_inputs.shape[0] // model.batch_size
+    total_acc = 0.0
+    all_preds = []
+
+    with torch.no_grad():
+        with tqdm(range(num_batches), desc="  test", unit="batch", leave=False,
+                  bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}]{postfix}") as pbar:
+            for batch_idx in pbar:
+                batch_inputs, batch_labels = get_next_batch(
+                    batch_idx, test_inputs, test_labels, model.batch_size
+                )
+
+                logits = model(batch_inputs)
+                batch_acc = model.accuracy(logits, batch_labels)
+
+                total_acc += batch_acc.item()
+                all_preds.append(logits)
+
+
+    test_accuracy = total_acc / num_batches
+    test_preds = torch.cat(all_preds, dim=0)
+
+    return test_accuracy, test_preds
 
 def main():
     '''
@@ -90,13 +127,26 @@ def main():
     # TODO: main() pt 3
     # Train your model for multiple epochs
 
+    num_epochs = 10
+    for epoch in range(num_epochs):
+        train_acc = train(model, optimizer, x_train, y_train)
+        print(f"Epoch {epoch + 1}/{num_epochs}, Train Accuracy: {train_acc:.4f}")
+        
     # TODO: main() pt 4
     # Test your model
+
+    test_acc, test_preds = test(model, x_test, y_test)
+    print(f"Test Accuracy: {test_acc:.4f}")
 
     # TODO: main() pt 5
     # Save your predictions as either "predictions_cnn.npy" or "predictions_mlp.npy"
     #   depending on which model you are using
-    return
+
+    pred_classes = torch.argmax(test_preds, dim=1)
+    np.save("predictions_mlp.npy", pred_classes.cpu().numpy())
+
+
+
 
 
 if __name__ == '__main__':
